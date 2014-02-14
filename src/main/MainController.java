581ad375126818b293277c19b3096ee7f123be68
chandrasekhar4u <chandu624@gmail.com>
2014-02-15 01:09:19 +0530

    converted to maven project.

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -6,18 +6,32 @@ import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author CHANDRA
+ * 
+ */
 public class MainController {
-	private static final ResourceBundle bundle = ResourceBundle.getBundle("configuration");
-	
+
+	/**
+	 * 
+	 */
+	private static final ResourceBundle bundle = ResourceBundle
+			.getBundle("configuration");
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		while (true) {
 			try {
-				if(!checkPattern(bundle.getString("FROM_EMAIL_ADDRESS").trim())) break;
-				if(!checkPattern(bundle.getString("TO_EMAIL_ADDRESS").trim())) break;
+				if (!checkPattern(bundle.getString("FROM_EMAIL_ADDRESS").trim()))
+					break;
+				if (!checkPattern(bundle.getString("TO_EMAIL_ADDRESS").trim()))
+					break;
 				try {
 					ImageUtil.capturedImage(); // return true or false
-					Thread.sleep((getDelayMin()*60*1000));
-				} catch(Exception e) {
+					Thread.sleep((getDelayMin() * 60 * 1000));
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			} catch (Exception e) {
@@ -25,19 +39,28 @@ public class MainController {
 			}
 		}
 	}
-	
+
+	/**
+	 * @return interval in minutes.
+	 */
 	private static int getDelayMin() {
 		return Integer.parseInt(bundle.getString("INTERVAL").trim());
 	}
-	
+
+	/**
+	 * @param word
+	 * @return
+	 */
 	private static boolean checkPattern(String word) {
 		try {
 			word = word.split("\\s")[0];
-			Pattern mailAddressPattern = Pattern.compile("[\\w\\.-]{3,30}@[\\w\\.-]{3,30}");
+			Pattern mailAddressPattern = Pattern
+					.compile("[\\w\\.-]{3,30}@[\\w\\.-]{3,30}");
 			Matcher eMailMatcher = mailAddressPattern.matcher(word);
-			if(eMailMatcher.find()) return true;
+			if (eMailMatcher.find())
+				return true;
 			return false;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
